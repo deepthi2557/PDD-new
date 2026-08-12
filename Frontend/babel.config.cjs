966d6520fc3cf92a207a1b97ab270eb18f4931b1
Deepthi Reddy <deepthi2557@gmail.com>
2026-08-12 20:42:33 +0530

    transform-import-meta-to-process-env

--- Frontend/babel.config.cjs
+++ Frontend/babel.config.cjs
@@ -2,5 +2,26 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      function({ types: t }) {
+        return {
+          visitor: {
+            MetaProperty(path) {
+              if (path.node.meta.name === 'import' && path.node.property.name === 'meta') {
+                if (path.parentPath.isMemberExpression() && path.parentPath.node.property.name === 'env') {
+                  const envPath = path.parentPath;
+                  envPath.replaceWith(
+                    t.memberExpression(
+                      t.identifier('process'),
+                      t.identifier('env')
+                    )
+                  );
+                }
+              }
+            }
+          }
+        };
+      }
+    ]
   };
 };
